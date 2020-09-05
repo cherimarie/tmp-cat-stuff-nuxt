28e1c794e1f5a590f1a815753c83f03a61436057
Dane Wallace <danewallace@icloud.com>
2020-09-04 18:30:26 -0700

    Implemented Flickr API in Social view and ImageCard component.

---
+++ components/ImageCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="item">
+    <a href="#">
+      <img class="image" :src="image.url_n" :alt="image.title" />
+      <div class="body">
+        <p v-if="image.title" class="image-title">{{ image.title }}</p>
+        <p v-else class="image-title">No Title Found</p>
+        <p class="image-owner">By {{ image.ownername }}</p>
+        <section class="image-date-view-wrapper">
+          <p class="image-date">{{ image.datetaken }}</p>
+          <p class="image-views">Views: {{ image.views }}</p>
+        </section>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCard',
+  props: ['image'],
+}
+</script>
+
+<style lang="scss">
+.item {
+  background-color: #eee;
+  display: inline-block;
+  margin: 0 0 1em;
+  width: 100%;
+}
+.item a {
+  text-decoration: none;
+}
+.item:hover .body {
+  visibility: visible;
+  opacity: 1;
+}
+.image {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+.body {
+  padding: 0.5rem 1rem 1rem;
+  position: relative;
+  height: 95px;
+  margin: -100px 0 0 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s, opacity 0.5s linear;
+}
+.image-title {
+  font-weight: bold;
+  margin: 0;
+}
+.image-owner {
+  margin-top: 0;
+  font-size: 0.8rem;
+}
+.image-title,
+.image-owner {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+.image-date-view-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.image-date,
+.image-views {
+  margin-bottom: 0;
+  font-size: 0.8rem;
+}
+</style>
--- pages/Social.vue
+++ pages/Social.vue
@@ -79,17 +79,26 @@
       </div>
     </div>
 
-    <div class="flickrGallery"></div>
+    <div id="page" class="wrapper">
+      <h4>Recent Flickr uploads</h4>
+      <p v-if="loading" class="text-centered">Loading...</p>
+      <div v-else class="masonry">
+        <image-card v-for="image in images" :key="image.id" :image="image" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import PageHeading from '@/components/PageHeading.vue'
+import ImageCard from '@/components/ImageCard'
+import axios from 'axios'
 
 export default {
   name: 'Social',
   components: {
     PageHeading,
+    ImageCard,
   },
   data() {
     return {
@@ -105,9 +114,80 @@ export default {
       kittenMode: false,
       activatedText: 'Kitten mode: Activated',
       deactivatedText: 'Kitten mode: Deactivated',
-      apiKey: 'd77d872ca3ee80854cc20e66d69ddf60',
-      photoset: '72157715847390777',
+      loading: false,
+      images: [],
     }
+  },
+  beforeMount() {
+    this.loadImages()
+  },
+  methods: {
+    loadImages() {
+      this.loading = true
+      this.fetchImages()
+        .then((response) => {
+          this.images = response.data.photoset.photo
+          this.loading = false
+        })
+        .catch((error) => {
+          console.log('An error ocurred: ', error)
+        })
+    },
+    fetchImages() {
+      return axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photosets.getPhotos',
+          api_key: 'd77d872ca3ee80854cc20e66d69ddf60',
+          photoset_id: '72157715847390777',
+          extras: 'url_n, url_o, owner_name, date_taken, views',
+          page: 1,
+          format: 'json',
+          nojsoncallback: 1,
+          per_page: 30,
+        },
+      })
+    },
   },
 }
 </script>
+
+<style lang="scss">
+.text-centered {
+  text-align: center;
+}
+.wrapper {
+  margin: 0 auto;
+  max-width: 1200px;
+  @media only screen and (max-width: 799px) {
+    max-width: 100%;
+    margin: 0 1.5rem;
+  }
+}
+.masonry {
+  margin: 1.5em auto;
+  max-width: 1200px;
+  column-gap: 1.5em;
+}
+
+@media only screen and (min-width: 1024px) {
+  .masonry {
+    column-count: 3;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .masonry {
+    column-count: 2;
+  }
+}
+
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .masonry {
+    column-count: 1;
+  }
+}
+</style>
